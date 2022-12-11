04b18cee84012d6826619ef67ecd572c14dbb220
GoncaloFonseca0 <113516785+GoncaloFonseca0@users.noreply.github.com>
2022-12-11 20:09:01 +0000

    second commit

--- shoppinglistapp/src/components/Product/index.js
+++ shoppinglistapp/src/components/Product/index.js
@@ -1,19 +1,25 @@
 import useAPI from "../../hook/useAPI";
-
+import button from "../button";
 const Product = ({
   handleOnclick,
+
   imgSrc,
   name,
   description,
   calories,
   id,
 }) => {
+  const saveUserData = () => {
+    alert("Click Happened");
+  };
   return (
     <div className={id}>
       <img alt="product img" src={imgSrc}></img>
       <p>{name}</p>
       <p>{description}</p>
-      <button onMouseOver={handleOnclick}>click me</button>
+      <button onMouseOver={handleOnclick} onClick={saveUserData}>
+        click me
+      </button>
     </div>
   );
 };
---
+++ shoppinglistapp/src/components/button/index.js
@@ -0,0 +1,12 @@
+// import Product from "../Product";
+
+// const button = ({handleOnClick}) => {
+//     const saveUserData = () => {
+//         alert("Click Happened");
+
+//     const onMouseOver = () => {
+//         onMouseOver = {handleOnClick};
+//     }
+//     }
+
+// export default buton;
